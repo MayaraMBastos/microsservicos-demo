9fe8986aa5f51e08b8dc678b1cfa89ec7feb4bf6
MayaraMB <maybastos2021@gmail.com>
2025-07-27 15:34:04 -0300

    feature: adicionado o serviço de transacoes

---
+++ transacao-service/src/test/java/com/maymb/microsservicos/transacao_service/TransacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maymb.microsservicos.transacao_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
